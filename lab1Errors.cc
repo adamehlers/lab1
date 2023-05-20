77e6f02e5553d6b565002b202baa53e698235eec
adamehlers <adamehlers15@gmail.com>
2023-05-20 17:15:59 -0400

    Initial commit

---
+++ lab1Errors.cc
@@ -0,0 +1,29 @@
+/**
+ *   @file: lab1Errors.cc
+ * @author: Enter your name
+ *   @date: Enter today's date
+ *  @brief: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+int main() {
+
+    double a, b, c;
+    double disc;
+    cout << "Enter the values for a, b, and c." << endl;
+    cin >> a >> b >> c
+    disc = sqrt(b * b - 4 * a * c);
+
+    double x1; x2;
+
+    x1 = (-b + disc) / (2 * a);
+    x2 = (-b - disc) / (2 * a);
+    cout << "x1 = ' << x1 << endl;
+    cout << "x2 = " << x2 << endl;
+
+    return 0;
+}

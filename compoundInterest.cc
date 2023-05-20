194e94b8308ae54ad4538547417c773e721f89d3
adamehlers <adamehlers15@gmail.com>
2023-05-20 17:56:48 -0400

    Initial commit

---
+++ compoundInterest.cc
@@ -0,0 +1,18 @@
+/**
+ *   @file: compoundInterest.cc
+ * @author: Adam Ehlers
+ *   @date: 05/20/2023
+ *  @brief: A program that calculates the future value of an investment using compound interest. I asks the user for an investment amount, the annual interest rate as a percentage, and the time period in years
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+
+int main(){
+
+
+
+}
